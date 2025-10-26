61898f184fbb7b0fe05cba1a5f99196899e50792
dramforever <dramforever@live.com>
2025-10-26 22:05:22 +0800

    [NOT-FOR-UPSTREAM] muvm-steam: init

---
+++ pkgs/by-name/mu/muvm-steam/package.nix
@@ -0,0 +1,36 @@
+{
+  bashInteractive,
+  config,
+  lib,
+  muvm,
+  path,
+  writeShellApplication,
+  writeShellScript,
+}:
+
+let
+  pkgsx86_64 = import path {
+    system = "x86_64-linux";
+    inherit config;
+  };
+
+  inherit (pkgsx86_64) steam mesa;
+  mesa32 = pkgsx86_64.pkgsi686Linux.mesa;
+
+  initScript = writeShellScript "muvm-steam-init.sh" ''
+    ln -snf ${mesa} /run/opengl-driver
+    ln -snf ${mesa32} /run/opengl-driver-32
+    ln -s ${steam} /run/steam
+    echo enable-shm=no > /run/pulse.conf
+    echo "Run steam with:"
+    echo "  PULSE_CLIENTCONFIG=/run/pulse.conf /run/steam/bin/steam"
+    echo
+  '';
+in
+
+writeShellApplication {
+  name = "muvm-steam";
+  text = ''
+    ${lib.getExe muvm} -x ${initScript} "''${SHELL:-${lib.getExe bashInteractive}}"
+  '';
+}
